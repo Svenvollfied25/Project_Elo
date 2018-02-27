2b7cd4df6f0fe163d588121160f5e5df95056a7c
Aman Tewary <aman.tewary@live.com>
2018-02-27 17:41:41 -0400

    Profile Page Layout and Activity

---
+++ app/src/main/java/group/hashtag/projectelo/Activities/TestActivity.java
@@ -0,0 +1,10 @@
+package group.hashtag.projectelo.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amant on 18/02/18.
+ */
+
+public class TestActivity extends AppCompatActivity {
+}
